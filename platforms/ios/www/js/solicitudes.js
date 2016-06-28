a01d4454c6c187bec6668dd975b9080334d74f7c
th3c3sar <cesar5_90@hotmail.com>
2016-06-27 21:24:19 -0500

    Full

---
+++ platforms/ios/www/js/solicitudes.js
@@ -0,0 +1,37 @@
+angular.module('starter.solicitudes', [])
+.factory('Solicitudes',function(){
+  var solicitudes=[{
+    id: 0,
+    name: 'Amazonas 12',
+    lastText: 'Callao - APM',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Hanjin 244',
+    lastText: 'Ilo - Enapu',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Hyundai 233',
+    lastText: 'Callao - DPW',
+    face: 'img/adam.jpg'
+  }];
+
+    return {
+    all: function() {
+      return solicitudes;
+    },
+    remove: function(solicitud) {
+      solicitudes.splice(solicitudes.indexOf(solicitud), 1);
+    },
+    get: function(SolicitudId) {
+      for (var i = 0; i < solicitudes.length; i++) {
+        if (solicitudes[i].id === parseInt(SolicitudId)) {
+          return solicitudes[i];
+        }
+      }
+      return null;
+    }
+  };
+
+  });
